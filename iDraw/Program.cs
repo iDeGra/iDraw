120297a5e716b4b39f120fc8626f54c0722a2990
Kinrany <kinrany@yandex.com>
2016-01-19 00:07:18 +0300

    ...

--- iDraw/Program.cs
+++ iDraw/Program.cs
@@ -5,15 +5,15 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace iDraw {
-    static class Program {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main() {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
+	static class Program {
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main() {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
 }
